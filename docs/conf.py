3af4750918590f798d83c2c3e4718b80ade61ea0
higgsas <59316820+higgsas@users.noreply.github.com>
2023-05-13 12:59:05 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'Higgsas Geometry nodes manual'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2023, Higgsas'
+author = 'Higgsas'
 
 release = '0.1'
 version = '0.1.0'
